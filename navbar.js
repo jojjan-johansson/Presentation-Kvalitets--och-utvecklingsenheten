9ebae1a08039c9c137710be422a29e50e6631ccd
Johanna Johansson <jojjan_johansson@hotmail.com>
2026-02-20 16:18:21 +0100

    En ny webb presentation

--- navbar.js
+++ navbar.js
@@ -5,11 +5,13 @@ document.addEventListener("DOMContentLoaded", () => {
             <nav>
                 <ul>
                     <li><a href="index.html">Start</a></li>
-                    <li><a href="jag.html">Om mig</a></li>
-                    <li><a href="resa.html">Resan</a></li>
-                    <li><a href="agilt.html">Agilt arbete</a></li>
+                    
+                    
+                    <li><a href="avgifter.html">Kvalitet</a></li>
+                    <li><a href="resa.html">Välfärdsteknik</a></li>
+                    <li><a href="jag.html">System</a></li>
+                    <li><a href="agilt.html">MAS, MAR, SAS</a></li>
                     <li><a href="treserva.html">Treserva-appen</a></li>
-                    <li><a href="avgifter.html">Avgiftshantering</a></li>
                     <li><a href="framtid.html">Framtiden</a></li>
                 </ul>
             </nav>
